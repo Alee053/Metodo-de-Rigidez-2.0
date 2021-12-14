b5d4bfeb493e948ddf029ab6942a954c6245741f
Ale <87765018+Alee053@users.noreply.github.com>
2021-12-14 19:02:14 -0400

    Prueba cambiar todo a vw

--- src/components/templates/Button.jsx
+++ src/components/templates/Button.jsx
@@ -1,15 +1,15 @@
 export default function Btn({
-  func = () => {
-    console.log("Button Clicked!");
-  },
-  children,
-  styles = "",
-  isDisabled = false,
-}) {
+                              func = () => {
+                                console.log("Button Clicked!");
+                              },
+                              children,
+                              styles = "",
+                              isDisabled = false,
+                            }) {
   return (
     <button
       className={
-        "text-white rounded-xl xl:p-2 xl:text-lg sm:text-sm hover:bg-gray-200 hover:bg-opacity-10 hover:text-white hover:shadow-md mx-5 disabled:opacity-50" +
+        "text-white rounded-xl p-2 text-lg hover:bg-gray-200 hover:bg-opacity-10 hover:text-white hover:shadow-md mx-5 disabled:opacity-50" +
         " " +
         styles
       }
